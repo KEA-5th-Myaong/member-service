aee42e3163b99fdb27f21af825f80f9e02098b35
strongmhk <kim6562166086@gmail.com>
2024-11-28 11:33:31 +0900

    🔨 [#8] fix : 일반 로그인시 NPE 예외 발생 방지

--- src/main/java/myaong/popolog/memberservice/jwt/CustomUserDetailService.java
+++ src/main/java/myaong/popolog/memberservice/jwt/CustomUserDetailService.java
@@ -1,7 +1,10 @@
 package myaong.popolog.memberservice.jwt;
 
 import lombok.RequiredArgsConstructor;
+import myaong.popolog.memberservice.converter.AuthConverter;
+import myaong.popolog.memberservice.converter.MemberConverter;
 import myaong.popolog.memberservice.entity.Member;
+import myaong.popolog.memberservice.enums.SocialType;
 import myaong.popolog.memberservice.repository.MemberRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -16,6 +19,7 @@ public class CustomUserDetailService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         Member findMember = memberRepository.findByUsername(username);
+        findMember = (findMember == null) ? new Member(username, null, null, SocialType.NORMAL,null, null, null, null, null) : findMember;
         return new CustomUserDetails(findMember);
     }
 }
