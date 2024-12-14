7cd72d3a815da034a3d57d5c130cffc2bd11ee92
strongmhk <kim6562166086@gmail.com>
2024-12-15 05:41:19 +0900

    🔧 chore: CORS URL ㅃㅏㄱ쳐서 바꿈

--- src/main/java/myaong/popolog/memberservice/config/WebConfig.java
+++ src/main/java/myaong/popolog/memberservice/config/WebConfig.java
@@ -26,7 +26,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", frontUrl, worker1Url, worker2Url)
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
                 .allowedHeaders("*") // 모든 HTTP 헤더 허용
                 .exposedHeaders("Authorization", "Set-Cookie") // 브라우저에 노출할 응답 헤더
